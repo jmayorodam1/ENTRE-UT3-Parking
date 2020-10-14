bd3e3ed4e314c2613a2130e0513efcb7f79fd507
Javier Mayor Oyarzun <jmayorodam1@gmail.com>
2020-10-14 19:59:36 +0200

    v

--- Parking.java
+++ Parking.java
@@ -20,43 +20,38 @@ public class Parking
     private String Tarifa;
     private double Importe;
     private int dia;
+    private int hora;
+    private double tarifa;
     /**
      * Inicializa el parking con el nombre indicada por el parámetro.
      * El resto de atributos se inicializan a 0 
      */
     public Parking() {
-       
+
         HoraLlegada = 0;
         HoraSalida = 0;
         NumeroCliente = 0;
         Tarifa = "";
         Importe = 0;
         nombreParking = "";
-        
-        
-        
-
-       
 
     }
-
     /**
      * accesor para el nombre del parking
      *  
      */
     public String getNombreParking() {
         return nombreParking;
-         
+
     }
-    
+
     /**
      * mutador para el nombre del parking
      *  
      */
     public void nombreParking (String queNombreParking) {
         nombreParking = queNombreParking;
-        
-        
+
     }
 
     /**
@@ -67,46 +62,53 @@ public class Parking
     public void entrada(int queHoraLlegada)
     {
         HoraLlegada = queHoraLlegada;
-        
+
     }
 
     /**
      * Mutador de salida
-   
+
      */
     public void salida(int queHoraSalida)
     {
         HoraSalida = queHoraSalida; 
-        
+
     }
 
     /**
      * Si es comercial o Regular
      *
-   
+
      */
-    public void Tarifa(String queTarifa){
-        Tarifa = queTarifa;
+    public void Tarifa(String tipoTarifa){
+        Tarifa = tipoTarifa;
         Regular = "R";
         Comercial = "C";
-       
+
     }
 
     /**
      * Dia de entrada
      *
-    
+
      */
     public void dia(int queDia)
     {
         dia = queDia;
-        
+
     }
 
+    /**
+     * 
+     *Tiempo en el parking
+     * 
+     */
+    public void hora(int queHora, int queHoraSalida ,int queHoraLlegada)
+    {
+        hora = queHora;
+        queHora = queHoraSalida - queHoraLlegada;
 
-
-
-    
+    }
 
     /**
      *  Recibe cuatro parámetros que supondremos correctos:
@@ -124,55 +126,53 @@ public class Parking
      *    
      *    (leer enunciado del ejercicio)
      */
-    public void facturarCliente(char tipoTarifa, int llegada, int salida, int dia) {
-        if (llegada){
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
-        
+    public void facturarCliente( char tipoTarifa, int queHoraEntrada, int queHoraSalida, int dia, int queHora) {
+        if (tipoTarifa = "R") { 
+            if (queHora < 11){
+                tarifa = 2 + ((3*queHora)*2);
+
+            }
+            if (queHora > 11){
+                tarifa = 2+ ((5*queHora)*2);
+
+            }
+
+            if (tipoTarifa = "C"){
+                if (queHora <= 3){
+                    tarifa = queHora *5;
+
+                }
+                if (queHora > 3){
+                    tarifa = (3*5) + (queHora*2) * 3; 
+
+                }
+
+            }
+
         
        
+        }
+        /**
+         * Muestra en pantalla https://github.com/jmayorodam1/ENTRE-UT3-Parking.gitlas estadísticcas sobre el parking  
+         *   
+         * (leer enunciado)
+         *  
+         */
+        private void printEstadísticas() {
 
-        
+            System.out.println ("Importe total entre todos los clientes" + tarifa  +  
+                "\nNº clientes tarifa regular" + numeroTarifaRegular  + 
+                "\nNºclientes tarifa comercial" + numeroTarifaComercial + 
+                "\nCliente tarifa COMERCIAL con factura maxima fue el" + "y pago");
+
+        }
+        /**
+         * 
+         *
+         *  Calcula y devuelve un String que representa el nombre del día
+         *  en el que más clientes han utilizado el parking - "SÁBADO"   "DOMINGO" o  "LUNES"
+         */
+        public String diaMayorNumeroClientes()
 
     }
-    
-
-    /**
-     * Muestra en pantalla https://github.com/jmayorodam1/ENTRE-UT3-Parking.gitlas estadísticcas sobre el parking  
-     *   
-     * (leer enunciado)
-     *  
-     */
-    public void printEstadísticas() {
-        
-        // System.out.println ("Importe total entre todos los clientes" + importeTotal  +  
-        // "\nNº clientes tarifa regular" + numeroTarifaRegular  +
-         // // "
-         // \nNºclientes tarifa comercial" + numeroTarifaComercial + 
-         // "\nCliente tarifa COMERCIAL con factura maxima fue el" xxx "y pago" xxx);
-         
-    }
-
-    /**
-     *  Calcula y devuelve un String que representa el nombre del día
-     *  en el que más clientes han utilizado el parking - "SÁBADO"   "DOMINGO" o  "LUNES"
-     */
-    // public String diaMayorNumeroClientes() {
-        
-        
-        
-        
-
-        
-
-    // }
-
-}
+   
